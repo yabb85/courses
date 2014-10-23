69101510add8fe4248d9878aefa7bca80f26dcbe
Laurent Hory <disciple@Bulbig.home>
2014-10-23 20:52:19 +0200

    add forgotten file

--- app/static/script.js
+++ app/static/script.js
@@ -1,85 +1,92 @@
-var id_list = window.location.pathname.split('/');
-var id_list = id_list[id_list.length-1];
+$(function() {
 
-var connection = new autobahn.Connection({
-	url: "ws://127.0.0.1:8080/ws",
-	realm: "realm1"
-});
+	var unfolded = false;
+	var id_list = window.location.pathname.split('/');
+	var id_list = id_list[id_list.length-1];
+	var connection = new autobahn.Connection({
+		url: "ws://127.0.0.1:8080/ws",
+		realm: "realm1"
+	});
 
-var unfolded = false;
+
+	connection.onopen = function (session) {
+		console.log("connected");
+
+		session.subscribe('refresh_add_product', function(prod) {
+			var li = $('#liste').find('li#'+prod[0]);
+			console.log(prod);
+			if(li.size()) {
+				li.find("span").html(prod[4]);
+			}else{
+				$('#liste').append('<li class="list-group-item panier" id="'+prod[0]+'">'+prod[2]+'<span class="badge">'+prod[4]+'</span></li>');
+			}
+		});
+
+		session.subscribe('refresh_remove_product', function(prod) {
+			$('#liste #'+prod).remove();
+		});
+
+		removeToList = function(product, liste) {
+			session.call('me.hory.remove_to_list', [product, liste]).then(
+				function(res){
+					console.log('product removed to list');
+				},
+				function(error){
+					console.log('impossible to remove product in list');
+				}		
+			);
+		}
+
+		newProduct = function(name, price, quantity, unit, img) {
+			session.call('me.hory.create_product', [name, price, quantity, unit, img]).then(
+				function(res) {
+					console.log('product created');
+				},
+				function(error) {
+					console.log('impossible to create product');
+				}
+			);
+		}
+	};
 
 
-connection.onopen = function (session) {
-	console.log("connected");
-
-	session.subscribe('refresh_add_product', function(prod) {
-		var li = $('#liste').find('li#'+prod[0])
-		if(li.size()) {
-			li.find("span").html(prod[4]);
-		}else{
-			$('#liste').append('<li class="achat" id="'+prod[0]+'" onclick=removeProduct('+prod[0]+','+prod[1]+')><img class="img_achat" src="/static/'+prod[3]+'" alt="'+prod[2]+'"/><span class="count_achat">'+prod[4]+'</span></li>');
+	$('#liste').on("click", ".panier", function() {
+		if (connection.session) {
+			connection.session.call('me.hory.remove_to_list', [this.id, id_list]).then(
+				function(res){
+					console.log('product removed to list');
+				},
+				function(error){
+					console.log('impossible to remove product in list');
+				}		
+			);
 		}
 	});
 
-	session.subscribe('refresh_remove_product', function(prod) {
-		$('#liste #'+prod).remove();
+
+	$('.achat').click(function() {
+		if (connection.session) {
+			connection.session.call('me.hory.add_to_list', [this.id, id_list]).then(
+				function(res){
+					console.log('product added to list');
+				},
+				function(error){
+					console.log('impossible to add product in list');
+				}		
+			);
+		}
 	});
 
-	addToList = function(product, liste, name) {
-		session.call('me.hory.add_to_list', [product, liste, name]).then(
-			function(res){
-				console.log('product added to list');
-			},
-			function(error){
-				console.log('impossible to add product in list');
-			}		
-		);
-	}	
-	
-	removeToList = function(product, liste) {
-		session.call('me.hory.remove_to_list', [product, liste]).then(
-			function(res){
-				console.log('product removed to list');
-			},
-			function(error){
-				console.log('impossible to remove product in list');
-			}		
-		);
-	}
 
-	newProduct = function(name, price, quantity, unit, img) {
-		session.call('me.hory.create_product', [name, price, quantity, unit, img]).then(
-			function(res) {
-				console.log('product created');
-			},
-			function(error) {
-				console.log('impossible to create product');
-			}
-		);
-	}
-};
+	$('#button_add').click(function() {
+		if($('#form_add_product').css('display') == "none") {
+			$('#form_add_product').css("display", "block");
+		} else {
+			$('#form_add_product').css("display", "none");
+		}
+		unfolded = !unfolded;
+	});
 
-connection.open();
 
-function addProduct(product, list_id, name) {
-	//$('#liste').append('<li>'+product+'</li>');
-	if(connection.session) {
-		addToList(product, list_id, name);
-	}
-};
-
-function removeProduct(product, list_id) {
-	//$('#liste').append('<li>'+product+'</li>');
-	if(connection.session) {
-		removeToList(product, list_id);
-	}
-};
-
-function unfold() {
-	if($('#form_add_product').css('display') == "none") {
-		$('#form_add_product').css("display", "block");
-	} else {
-		$('#form_add_product').css("display", "none");
-	}
-	unfolded = !unfolded;
-};
+	connection.open();
+});
